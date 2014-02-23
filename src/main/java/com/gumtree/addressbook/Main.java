8313c492e74c7a06cdea12c23f29dbb6c2203546
kkachel <krzychu.kachel@gmail.com>
2014-02-23 22:39:03 +0100

    Initialize Spring context and read address book.

--- src/main/java/com/gumtree/addressbook/Main.java
+++ src/main/java/com/gumtree/addressbook/Main.java
@@ -1,5 +1,12 @@
 package com.gumtree.addressbook;
 
+import com.gumtree.addressbook.reader.AddressBookReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 /**
  * Application start class.
  *
@@ -7,7 +14,16 @@ package com.gumtree.addressbook;
  */
 public class Main {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("context/application-context.xml");
+            AddressBookReader addressBookReader = (AddressBookReader) context.getBean("addressBookReader");
+            addressBookReader.read();
+        } catch(Exception e) {
+            LOG.error("Error during main execution", e);
+        }
     }
 
 }
