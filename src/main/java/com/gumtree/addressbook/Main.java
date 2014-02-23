ebca0a5dad0928ee56ca30da57a90999daa9d4e1
kkachel <krzychu.kachel@gmail.com>
2014-02-23 18:55:07 +0100

    Setting up project.

---
+++ src/main/java/com/gumtree/addressbook/Main.java
@@ -0,0 +1,13 @@
+package com.gumtree.addressbook;
+
+/**
+ * Application start class.
+ *
+ * @author Krzysztof Kachel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
